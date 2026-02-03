c9e2650a82e09cb09dc2dc8b0c328e499e67a8b9
Tony Milanzi <tonymilanzi01@gmail.com>
2026-02-03 14:19:14 +0200

    Fixed import error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
-import { AuthProvider } from "./lib/auth";
+import { AuthProvider } from "./app/lib/auth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
